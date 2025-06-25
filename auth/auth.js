418081bcb2cb01c39ed9e3483b33e5f519cd10bc
nanamigithub <yi1a2b3c45@gmail.com>
2025-06-25 18:17:19 +0900

    登録、signupmail

--- auth/auth.js
+++ auth/auth.js
@@ -3,7 +3,8 @@ import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 const supabaseUrl = 'https://iwdyzfsunnyqcwipttql.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml3ZHl6ZnN1bm55cWN3aXB0dHFsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA1ODk1NzAsImV4cCI6MjA2NjE2NTU3MH0.A9b8BxZLeFK1ZPeIBwSppn8r8Oz9nIN1pR8cDqAreH8';  // 请替换为有效KEY
  // ⚠️ 本番用には環境変数で管理
-export default supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = createClient(supabaseUrl, supabaseKey);
+export default supabase;
 
 document.getElementById("loginBtn")?.addEventListener("click", async () => {
   const email = document.getElementById("email").value;
